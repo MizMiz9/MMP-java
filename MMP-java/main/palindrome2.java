ba331d9577e517ce670e967df692d11827c110b9
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2024-01-12 12:56:37 +0800

    Add files via upload

--- MMP-java/main/palindrome2.java
+++ MMP-java/main/palindrome2.java
@@ -1,7 +1,21 @@
 package main;
 
+import java.util.Scanner;
+
 public class palindrome2 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String userinput = scan.nextLine();
         
+        StringBuilder bl1 = new StringBuilder(userinput);
+        bl1.reverse();
+        String x = bl1.toString();
+        System.out.println(bl1);
+        if (userinput.equals(x)) {
+            System.out.println("Palindrome detected! ");
+        }else{
+            System.out.println("Good name");
+        }
     }
 }
